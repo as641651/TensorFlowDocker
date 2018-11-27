6c8c8f0fb964591eae140c952d653463bf68248e
Aravind Sankaran <aravind.sankaran@rwth-aachen.de>
2018-11-27 23:38:13 +0530

    Added dataset API

--- run.sh
+++ run.sh
@@ -1,1 +1,2 @@
-docker run -it -p 8000:8888 -p 0.0.0.0:6005:6006 -v ${1:-$PWD}:/home --runtime=nvidia senesence/tensorflow:1.12.0-gpu-py3 $2
+#docker run -it -p 8000:8888 -p 0.0.0.0:6005:6006 -v ${1:-$PWD}:/home --runtime=nvidia senesence/tensorflow:1.12.0-gpu-py3 $2
+docker run -it -p 8000:8888 -p 0.0.0.0:6005:6006 -v ${1:-$PWD}:/home -v $HOME/Image_data/VOC2007:/home/exercise/datasets/VOC2007 --runtime=nvidia senesence/tensorflow:1.12.0-gpu-py3 $2
